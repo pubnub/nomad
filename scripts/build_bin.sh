e4c810dce5d503bab21ee499e01c81f1d72d35f7
Jordan Shaw <jordan@pubnub.com>
2016-04-30 05:08:23 -0700

    reworked build_bin.sh
    Signed-off-by: Jordan Shaw <jordan@pubnub.com>

--- scripts/build_bin.sh
+++ scripts/build_bin.sh
@@ -2,10 +2,14 @@
 sudo apt-get update
 sudo apt-get install -y build-essential curl git-core bzr libpcre3-dev pkg-config zip default-jre qemu libc6-dev-i386 silversearcher-ag jq unzip
 
-PWD=$( pwd )
+DATE=`date +"%Y-%m-%d-%H-%M-%S"`
+CURRENT_DIR=`pwd`
+
 # Setup go, for development of Nomad
-SRCROOT="$PWD/go"
-SRCPATH="$PWD/gopath"
+BUILDROOT="/tmp"
+NOMADBUILDPATH="$BUILDROOT/nomad-$DATE"
+SRCROOT="$BUILDROOT/go"
+SRCPATH="$NOMADBUILDPATH/gopath"
 
 # Get the ARCH
 ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
@@ -15,30 +19,27 @@ if [ -d $SRCPATH ] ; then
     rm -rf $SRCPATH
 fi
 
-# ideally don't reinstall go each time but whatever
-if [ -d $SRCROOT ] ; then
-    rm -rf $SRCROOT
-fi
-
 # Setup the GOPATH; even though the shared folder spec gives the working
 # directory the right user/group, we need to set it properly on the
 # parent path to allow subsequent "go get" commands to work.
-sudo mkdir -p $SRCPATH
-sudo mkdir -p $SRCPATH/src/github.com/hashicorp/nomad
+mkdir -p $SRCPATH
+mkdir -p $SRCPATH/src/github.com/hashicorp/nomad
 
-# a cp should work here but it's not so doing this is the mean time
-git clone --branch feature/blocks/v0.3.2 https://github.com/pubnub/nomad.git $SRCPATH/src/github.com/hashicorp/nomad
+# ideally don't reinstall go each time but whatever
+if [ ! -d $SRCROOT ] ; then
+    # Install Go
+    wget -P $BUILDROOT -q https://storage.googleapis.com/golang/go1.6.linux-${ARCH}.tar.gz
+    tar -xf $BUILDROOT/go1.6.linux-${ARCH}.tar.gz -C $BUILDROOT 
+    chmod 775 $SRCROOT
+fi
 
-# Install Go
-cd /tmp
-wget -q https://storage.googleapis.com/golang/go1.6.linux-${ARCH}.tar.gz
-tar -xf go1.6.linux-${ARCH}.tar.gz
-sudo mv go $SRCROOT
-sudo chmod 775 $SRCROOT
-sudo chown ubuntu:ubuntu $SRCROOT
+# move stuff from workspace to /tmp/nomad-* directory
+echo "cp -rf $CURRENT_DIR/* $SRCPATH/src/github.com/hashicorp/nomad" 
+cp -rf $CURRENT_DIR/* $SRCPATH/src/github.com/hashicorp/nomad
 
-sudo chown -R ubuntu:ubuntu $SRCPATH 3>/dev/null || true
-# ^^ silencing errors here because we expect this to fail for the shared folder
+# make bin requires this T_T
+echo "cp -rf $CURRENT_DIR/.git $SRCPATH/src/github.com/hashicorp/nomad" 
+cp -rf $CURRENT_DIR/.git $SRCPATH/src/github.com/hashicorp/nomad
 
 cat <<EOF >/tmp/gopath.sh
 export GOPATH="$SRCPATH"
@@ -56,6 +57,11 @@ cd $SRCPATH/src/github.com/hashicorp/nomad && make bootstrap
 bash scripts/install_rkt.sh
 
 XC_ARCH=amd64 XC_OS=linux make bin
+
+# move binary to workspace
+mv $SRCPATH/src/github.com/hashicorp/nomad/pkg $CURRENT_DIR/pkg-$DATE
+# cleanup
+rm -rf $NOMADBUILDPATH
 
 # CD into the nomad working directory when we login to the VM
 grep "cd $SRCPATH/src/github.com/hashicorp/nomad" ~/.profile || echo "cd $SRCPATH/src/github.com/hashicorp/nomad" >> ~/.profile
